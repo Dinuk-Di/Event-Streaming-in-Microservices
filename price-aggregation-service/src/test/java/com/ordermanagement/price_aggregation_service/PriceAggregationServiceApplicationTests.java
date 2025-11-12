07e5fe3f11a32e68755a4ec16c3c03f8c91b7d1f
Dinuk-Di <dinukpkcc@gmail.com>
2025-11-12 18:03:50 +0530

    microservices initialized

---
+++ price-aggregation-service/src/test/java/com/ordermanagement/price_aggregation_service/PriceAggregationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordermanagement.price_aggregation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceAggregationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
